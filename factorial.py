dfa302c25a67a82b7347509d0f8f05ceb159a923
chilemariiee <elihceiram@gmail.com>
2018-02-19 13:33:55 +0800

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,16 @@
+
+
+num = input("Enter number: ")
+factorial = 1
+
+if num < 0:
+    print "Must be positive"
+
+elif num == 0:
+        print "factorial = 1"
+
+else:
+    for i in range (1, num+1):
+            factorial = factorial*i
+    print "factorial number is", num, "and value is", factorial
+            
